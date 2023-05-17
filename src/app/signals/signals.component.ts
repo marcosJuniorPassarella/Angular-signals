f052b6393a8eb472a52b63ae3583b980f1d63ec4
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-05-17 19:00:48 -0300

    Implement new signals changes

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -8,20 +8,26 @@ import { Component, signal } from "@angular/core";
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  actions = signal<Array<string>>([]);
   counter = signal(0);
 
-  // set() para definir um novo valor
+  // set() para definir um novo valor que não seja baseado no valor antigo
   // update((oldValue) => oldValue + 1) para definir um novo valor com base em um valor antigo
-  // mutate() para atualizar o valor com base em um valor antigo
+  // mutate() para atualizar o valor com base em um valor antigo, e esse método deve ser usado...
+  //para valores que podem sofrer mutação, o que não é o caso dos números.
+  // o mutate() nos permite atribuir um novo valor mas editando um valor existente
+  // Ações de push de arrays só devem ser utilizadas com o método mutate()
 
   increment() {
+    // Exemplo de uso dos métodos do Signal
+    // this.counter.set(this.counter() + 1);
     this.counter.update((oldCounter) => oldCounter + 1);
-    this.actions.push("INCREMENT");
+    this.actions.mutate((oldActions) => oldActions.push("INCREMENT"));
+    // Alternativa => this.actions.update((oldActions) => [...oldActions, 'INCREMENT']);
   }
 
   decrement() {
     this.counter.update((oldConter) => oldConter - 1);
-    this.actions.push("DECREMENT");
+    this.actions.mutate((oldActions) => oldActions.push("DECREMENT"));
   }
 }
